0104917defc2e0e4cfff08531b65b62b59d0be6f
Faiz Shaikh <mohdfaiz8101@gmail.com>
2025-06-04 16:27:28 +0530

    fixes on links

--- app/ui/components/Skills.tsx
+++ app/ui/components/Skills.tsx
@@ -1,12 +1,14 @@
-// import Image from "next/image"
+import clsx from 'clsx';
 import { motion, useAnimation } from 'framer-motion'
-// import { useEffect, useState } from "react"
+import { useState } from "react"
 
 export default function Skills() {
     const controls = useAnimation()
     const frontendSkills = ['React', 'NextJs', 'JavaScript', 'TypeScript', 'TailwindCSS',]
     const backendSkills = ['Nodejs', 'Expressjs']
     const toolsServices = ['MongoDB', 'Postman', 'Git', 'Github', 'Appwrite']
+    const [isHovered, setIsHovered] = useState(false);
+
 
     return (
         <motion.div
@@ -22,8 +24,11 @@ export default function Skills() {
             animate={controls}
             className="flex border h-fit border-neutral-800/50 z-0 rounded-2xl flex-col p-0.5 cursor-grab"
             style={{ touchAction: 'none' }}
+
         >
-            <div className="border space-y-8 h-full w-[18rem] sm:p-4 p-2 bg-[radial-gradient(circle_at_15%_18%,_rgba(255,255,255,0.09)_5%,_#111111_25%)] border-neutral-800/50 z-1 rounded-2xl">
+            <div onMouseEnter={() => setIsHovered(true)}
+                onMouseLeave={() => setIsHovered(false)}
+                className="border space-y-4 h-full w-[18rem] sm:p-4 p-2 bg-[radial-gradient(circle_at_15%_18%,_rgba(255,255,255,0.09)_5%,_#111111_25%)] border-neutral-800/50 z-1 rounded-2xl">
                 <div className='flex flex-col text-6xl gap-4 font-bold'>
                     <div className='flex gap-3 hover:gap-6 transition-all duration-300 ease-in-out'>
                         <span>{"{"}</span><span>{"}"}</span>
@@ -34,58 +39,66 @@ export default function Skills() {
                     </div>
                 </div>
 
-                <div className="group relative h-2 w-full bg-neutral-800 rounded-2xl overflow-hidden">
-                    <div className="absolute left-0 top-0 h-full bg-white w-0 opacity-0 group-hover:opacity-100 group-hover:w-full transition-all duration-1000 ease-in-out"></div>
+                <div className="group relative h-2 w-full rounded-2xl overflow-hidden">
+                    <div
+                        className={clsx(
+                            'absolute left-0 top-0 h-full bg-white transition-all duration-1000 ease-in-out',
+                            isHovered ? 'opacity-100 w-full' : 'opacity-0 w-0'
+                        )}
+                    />
                 </div>
 
+                <div className='space-y-6'>
 
-                <div className='space-y-1.5'>
-                    <p className='text-lg font-bold'>Frontend:</p>
-                    <div className='flex flex-wrap gap-2 text-sm font-bold'>
-                        {frontendSkills.map((item) => (
-                            <span
-                                key={item}
-                                className='flex border border-neutral-800/80 z-0 rounded-lg flex-col p-0.5 cursor-grab hover:border-white/50'
-                            >
-                                <span className="flex border px-2 bg-[#181717] border-neutral-800/50 z-0 rounded-lg flex-col p-0.5 cursor-grab">
-                                    {item}
+                    <div className='space-y-1.5'>
+                        <p className='text-lg text-gray-300 font-bold'>Frontend:</p>
+                        <div className='flex flex-wrap gap-2 text-gray-400 text-sm font-bold'>
+                            {frontendSkills.map((item) => (
+                                <span
+                                    key={item}
+                                    className='flex border border-neutral-800/80 z-0 rounded-xl flex-col p-0.5 cursor-grab hover:border-white/50'
+                                >
+                                    <span className="flex border px-2 bg-[#181717] border-neutral-800/50 z-0 rounded-lg flex-col p-0.5 cursor-grab">
+                                        {item}
+                                    </span>
                                 </span>
-                            </span>
-                        ))}
+                            ))}
+                        </div>
+                    </div>
+
+                    <div className='space-y-1.5'>
+                        <p className='text-lg text-gray-300 font-bold'>Backend:</p>
+                        <div className='flex flex-wrap gap-2 text-gray-400 text-sm font-bold'>
+                            {backendSkills.map((item) => (
+                                <span
+                                    key={item}
+                                    className='flex border border-neutral-800/80 z-0 rounded-lg flex-col p-0.5 cursor-grab hover:border-white/50'
+                                >
+                                    <span className="flex border px-2 bg-[#181717] border-neutral-800/50 z-0 rounded-lg flex-col p-0.5 cursor-grab">
+                                        {item}
+                                    </span>
+                                </span>
+                            ))}
+                        </div>
+                    </div>
+
+                    <div className='space-y-1.5'>
+                        <p className='text-lg text-gray-300 font-bold'>Tools & Services:</p>
+                        <div className='flex flex-wrap gap-2 text-gray-400 text-sm font-bold'>
+                            {toolsServices.map((item) => (
+                                <span
+                                    key={item}
+                                    className='flex border border-neutral-800/80 z-0 rounded-lg flex-col p-0.5 cursor-grab hover:border-white/50'
+                                >
+                                    <span className="flex border px-2 bg-[#181717] border-neutral-800/50 z-0 rounded-lg flex-col p-0.5 cursor-grab">
+                                        {item}
+                                    </span>
+                                </span>
+                            ))}
+                        </div>
                     </div>
                 </div>
 
-                <div className='space-y-1.5'>
-                    <p className='text-lg font-bold'>Backend:</p>
-                    <div className='flex flex-wrap gap-2 text-sm font-bold'>
-                        {backendSkills.map((item) => (
-                            <span
-                                key={item}
-                                className='flex border border-neutral-800/80 z-0 rounded-lg flex-col p-0.5 cursor-grab hover:border-white/50'
-                            >
-                                <span className="flex border px-2 bg-[#181717] border-neutral-800/50 z-0 rounded-lg flex-col p-0.5 cursor-grab">
-                                    {item}
-                                </span>
-                            </span>
-                        ))}
-                    </div>
-                </div>
-
-                <div className='space-y-1.5'>
-                    <p className='text-lg font-bold'>Tools & Services:</p>
-                    <div className='flex flex-wrap gap-2 text-sm font-bold'>
-                        {toolsServices.map((item) => (
-                            <span
-                                key={item}
-                                className='flex border border-neutral-800/80 z-0 rounded-lg flex-col p-0.5 cursor-grab hover:border-white/50'
-                            >
-                                <span className="flex border px-2 bg-[#181717] border-neutral-800/50 z-0 rounded-lg flex-col p-0.5 cursor-grab">
-                                    {item}
-                                </span>
-                            </span>
-                        ))}
-                    </div>
-                </div>
             </div>
         </motion.div>
     )
