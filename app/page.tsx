2a451f2b65a7de170cfdd11dbc6d15ff03238bde
Faiz Shaikh <mohdfaiz8101@gmail.com>
2025-06-05 12:18:56 +0530

    grid-added

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,14 @@ import ProjectsWrapper from './ui/components/ProjectsWrapper'
 
 export default function Home() {
   return (
-    <div className="flex flex-col md:flex-row md:justify-center flex-wrap gap-6 p-4">
+    <div className="flex flex-col md:flex-row md:justify-center flex-wrap min-h-screen w-full z-0 relative gap-6 p-4">
+      <div className='absolute inset-0 z-0'
+        style={{
+          backgroundImage:
+            `linear-gradient(to right, #181717 1px, transparent 1px),
+          linear-gradient(to bottom, #181717 1px, transparent 1px)`,
+          backgroundSize: '40px 40px'
+        }} />
       <SkillsWrapper />
       <div className="flex flex-col gap-6 w-full md:w-auto">
         <HeroWrapper />
